b1c53acc4786cd0de9188369fdde505fe69200d2
mk8505 <baghel.manoj78@gmail.com>
2024-11-06 14:55:10 +0530

    test adding multiple numbers separated by commas.

--- calculator.js
+++ calculator.js
@@ -1,9 +1,9 @@
 function add(numbers) {
     if (numbers === '') return 0;
-    /*return number */
-    return parseInt(numbers);;
-  }
-  
-  module.exports = { add };
-  
-  
+    /*return a string of comma-separated numbers, processing that string into an array of integers and retune single array value */
+    const nums = numbers.split(',').map(num => parseInt(num));
+    return nums.reduce((sum, num) => sum + num, 0);
+}
+
+module.exports = { add };
+
--- calculator.test.js
+++ calculator.test.js
@@ -1,13 +1,22 @@
 const { add } = require('./calculator');
-
+/*step 1 empty string*/
 describe('String Calculator', () => {
-  test('should return 0 for an empty string', () => {
-    expect(add('')).toBe(0);
-  });
+    test('should return 0 for an empty string', () => {
+        expect(add('')).toBe(0);
+    });
 });
-
+/*step 2 a single number*/
 test('should return the number for a single number', () => {
     expect(add('1')).toBe(1);
     expect(add('5')).toBe(5);
-  });
-  
+});
+/*step 3 two numbers or multiple numbers */
+test('should return the sum of two numbers', () => {
+    expect(add('1,5')).toBe(6);
+});
+
+test('should return the sum of multiple numbers', () => {
+    expect(add('1,2,3')).toBe(6);
+});
+
+
